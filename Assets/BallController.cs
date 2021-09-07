b65e521b7c29ba1ed4a488041c6e4703de374822
erzhanto <erzhariessfeld@gmail.com>
2021-09-08 02:32:47 +0700

    Unity Pong Agate Course

---
+++ Assets/BallController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody2D bola;
+    public float xInitialForce;
+    public float yInitialForce;
+    private Vector2 trajectoryOrigin;
+    private float spd = 5.0f;
+    void ResetBall()
+    {
+        // Reset posisi menjadi (0,0)
+        transform.position = Vector2.zero;
+
+        // Reset kecepatan menjadi (0,0)
+        bola.velocity = Vector2.zero;
+    }
+    void PushBall()
+    {
+        // Tentukan nilai komponen y dari gaya dorong antara -yInitialForce dan yInitialForce
+        float yRandomInitialForce = Random.Range(0 , 2);
+
+        // Tentukan nilai acak antara 0 (inklusif) dan 2 (eksklusif)
+        float randomDirection = Random.Range(0, 2);
+
+        // Jika nilainya di bawah 1, bola bergerak ke kiri. 
+        // Jika tidak, bola bergerak ke kanan.
+        if (randomDirection < 1.0f)
+        {
+            // Gunakan gaya untuk menggerakkan bola ini.
+            bola.AddForce(new Vector2(-xInitialForce, yRandomInitialForce));
+
+        }
+        else
+        {
+            bola.AddForce(new Vector2(xInitialForce, yRandomInitialForce));
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        trajectoryOrigin = transform.position;
+    }
+    public Vector2 TrajectoryOrigin
+    {
+        get { return trajectoryOrigin; }
+    }
+    void RestartGame()
+    {
+        // Kembalikan bola ke posisi semula
+        ResetBall();
+
+        // Setelah 2 detik, berikan gaya ke bola
+        Invoke("PushBall", 2);
+    }
+    void Start()
+    {
+        bola = GetComponent<Rigidbody2D>();
+        trajectoryOrigin = transform.position;
+
+        // Mulai game
+        RestartGame();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float spd = 10.0f;
+    public float batas = 9.0f;
+    private Rigidbody2D raket;
+    private int skor;
+    public KeyCode upButton = KeyCode.W;
+    public KeyCode downButton = KeyCode.S;
+    private ContactPoint2D lastContactPoint;
+
+    public ContactPoint2D LastContactPoint
+    {
+        get { return lastContactPoint; }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Equals("Ball"))
+        {
+            lastContactPoint = collision.GetContact(0);
+        }
+    }
+
+
+    void Start()
+    {
+        raket = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        Vector2 velocity = raket.velocity;
+        if (Input.GetKey(upButton))
+        {
+
+            velocity.y = spd;
+        }
+        else if (Input.GetKey(downButton))
+        {
+            velocity.y = -spd;
+        }
+        else
+        {
+            velocity.y = 0.0f;
+        }
+        raket.velocity = velocity;
+
+        Vector3 posisi = transform.position;
+        if (posisi.y > batas)
+        {
+            posisi.y = batas;
+        }
+        else if (posisi.y < -batas)
+        {
+            posisi.y = -batas;
+        }
+        transform.position = posisi;
+    }
+    public void skornaik()
+    {
+        skor++;
+    }
+    public void resetskor()
+    {
+        skor = 0;
+    }
+    public int Skor
+    {
+        get { return skor; }
+    }
+}
